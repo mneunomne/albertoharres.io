6f9728b25da312f812c5273cb6dacf2c838b5c99
alberto venosa <alberto.harres@gmail.com>
2020-01-14 11:45:20 -0300

    audios

--- src/works/Carruagens.vue
+++ src/works/Carruagens.vue
@@ -5,10 +5,29 @@
     <p class='row'>
       <expandable-image :close-on-background-click="true" src="assets/carruagens/museu-historico-nacional.jpg"/>
     </p>
+    <p class='row'>
+      <AudioBox
+        v-for="(audio, index) in audios"
+        :key="index"
+        :filename="'assets/vozesdaterra/xoco-unknown.mp3'"
+        :title="'Xocó'"
+        :id="'1'"
+      ></AudioBox>
+    </p>
   </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      audios: [
+        'assets/carruagens/BOM taua-bancario-tia-vai.mp3',
+        'assets/carruagens/marecha650-duplo.mp3',
+        'assets/carruagens/parque-real-tubiacanca-com-voz.mp3',
+        'assets/carruagens/vem-italiano-vem-fazenda.mp3',
+      ]
+    }
+  },
   // work info
   tags: [
     'arduino',
