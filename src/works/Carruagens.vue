f1c7deb47577258bd1a9afabd6529ac3e32c87ea
alberto venosa <alberto.harres@gmail.com>
2020-01-14 18:23:44 -0300

    fix

--- src/works/Carruagens.vue
+++ src/works/Carruagens.vue
@@ -3,28 +3,49 @@
     <div class="row" v-html="$t('carruagens.title')"/>
     <div class="row" v-html="$t('carruagens.text')"/>
     <p class='row'>
-      <image-gallery :imgs="['assets/carruagens/museu-historico-nacional.jpg']" :cols="1"/>
+      <image-gallery :imgs="[
+      'assets/carruagens/museu-historico-nacional.jpg',
+      'assets/carruagens/carruagens_thumb.jpg'
+      ]" :cols="2"/>
     </p>
     <p class='row'>
       <AudioBox
         v-for="(audio, index) in audios"
         :key="index"
-        :filename="'assets/vozesdaterra/xoco-unknown.mp3'"
-        :title="'Xocó'"
-        :id="'1'"
+        :filename="audio.mp3"
+        :title="audio.title"
+        :id="''+ index"
       ></AudioBox>
     </p>
   </div>
 </template>
 <script>
+
+import AudioBox from '../components/AudioBox.vue'
+
 export default {
+  components: {
+    AudioBox
+  },
   data () {
     return {
       audios: [
-        'assets/carruagens/BOM taua-bancario-tia-vai.mp3',
-        'assets/carruagens/marecha650-duplo.mp3',
-        'assets/carruagens/parque-real-tubiacanca-com-voz.mp3',
-        'assets/carruagens/vem-italiano-vem-fazenda.mp3',
+        {
+          title: 'Tauá Bancário',
+          mp3: 'assets/carruagens/BOM taua-bancario-tia-vai.mp3',
+        },
+        {
+          title: 'Marechal',
+          mp3: 'assets/carruagens/marecha650-duplo.mp3',
+        },
+        {
+          title: 'Parque Real Tubiacanga',
+          mp3: 'assets/carruagens/parque-real-tubiacanca-com-voz.mp3',
+        },
+        {
+          title: 'Italiano Fazenda Quilombo',
+          mp3: 'assets/carruagens/vem-italiano-vem-fazenda.mp3',
+        },
       ]
     }
   },
