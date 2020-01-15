5e656b5d760a2fa88de0b89ef50f1bc1d87d1101
alberto venosa <alberto.harres@gmail.com>
2020-01-15 12:54:44 -0300

    ajuste grid

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -225,4 +225,10 @@ export default {
     width: calc(25vw - 40px) !important
     height: calc(25vw - 40px) !important
 
+@media (min-width: 1600px)
+  .box-fake
+    margin-bottom: 3px
+    width: calc(20vw - 28px) !important
+    height: calc(20vw - 28px) !important
+
 </style>
